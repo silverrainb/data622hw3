318e476a80729cbd550fa8ab7104724ecbf9796b
silverrainb <62fb1d0a@opayq.com>
2018-10-14 04:07:51 +0900

    changing dir path

--- start.sh
+++ start.sh
@@ -2,7 +2,8 @@
 
 cd /usr/src/app/data622hw3
 git pull
-python3 /usr/src/app/data622hw3/titanic/main.py
+cd titanic
+python3 main.py
 
 #docker pull silverrainb/titanic
 #docker run -it --rm --name trading-app --network mongo-network silverrainb/crypto-forecast
