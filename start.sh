f4daa9adb591c1e84e30c11acfbe14b387dd1086
silverrainb <silverrainb@users.noreply.github.com>
2018-10-14 20:51:29 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,3 @@ cd /usr/src/app/data622hw3
 git pull
 cd titanic
 python3 main.py
-
-#docker pull silverrainb/titanic
-#docker run -it --rm --name trading-app --network mongo-network silverrainb/crypto-forecast
